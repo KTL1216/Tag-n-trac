c2eb7521b87e875482a9412ff80d19e9bfe62bc8
unknown <kentheolee@gmail.com>
2024-07-26 15:20:09 -0700

    pyvisa commands

--- Temperature Chamber Test/temp_chamber_test.py
+++ Temperature Chamber Test/temp_chamber_test.py
@@ -1,8 +1,32 @@
 import pyvisa
 
-# Create a resource manager
-rm = pyvisa.ResourceManager()
+try:
+    # Create a resource manager
+    rm = pyvisa.ResourceManager()
 
-# List available resources
-resources = rm.list_resources()
-print('Available resources:', resources)
+    # Replace with your device's resource string
+    resource_string = 'USB::0x0957::0x0807::MY1234567::INSTR'
+
+    # Open a connection to the device
+    instrument = rm.open_resource(resource_string)
+
+    # Query the instrument's identity (IDN)
+    idn_response = instrument.query('*IDN?')
+    print('Instrument ID:', idn_response)
+
+    # Example: Set channel and read data (replace with appropriate commands for your device)
+    # Set the measurement configuration (example command, consult your device's manual)
+    instrument.write('CONF:VOLT:DC AUTO')
+    instrument.write('ROUT:CHAN:OPEN:ALL')  # Open all channels
+    instrument.write('ROUT:CHAN:CLOS (@101)')  # Close channel 101
+
+    # Read the data
+    data = instrument.query('READ?')
+    print('Measurement data:', data)
+
+    # Close the connection
+    instrument.close()
+except pyvisa.VisaIOError as e:
+    print(f'VISA I/O Error: {e}')
+except Exception as e:
+    print(f'Error: {e}')
